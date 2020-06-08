05c6bfc36423ac649fd0a40cb21939ba0b13f592
Lorenzo Rosmarino <lorenzo.rosmarino@epitech.eu>
2020-06-08 22:00:14 +0200

    Added final bash

--- ex02/bash/crack-me.sh
+++ ex02/bash/crack-me.sh
@@ -1,37 +1,26 @@
 #!/usr/bin/echo No such file or directory: 
 
 key="Imp0st3r_4_T34m_M4gm4"
-#   ".x.xx..x..........xxx"
-relic="ADIcAAVHbDAtVlQbbFQzNG1fTTRnbTQ="
+#   ".x..x..x..........xx."
+relic="ADIcAAVHbDAtBDs9ADRtX000Z200"
 
-assert_eq() { echo $1 $2 ;  [ "$1" != "$2" ] && exit 1; }
-
+assert_eq() { [ "$1" != "$2" ] && exit 1; }
 
 assert_eq `tr -dc '_' <<< "$key" | wc -c` 3
-assert_eq ${#key} 21
 assert_eq ${key:0:1} I
-assert_eq `head -c1<<<"printf Ditto"` ${key:2:1}
-
-M=4M_M4; assert_eq ${M/M/m} ${key:13:5}
 assert_eq `printf '%x\n' "'${key:17:1}"` ${key:12:2}
-
-echo --\> ${key:6:1}${key:9:1} - ${key:12:2} = ${key:3:1}
-echo --\> 4connu - 4inconnu = ${key:3:1}
-
-assert_eq ${key:5:1} ${relic:11:1}
+assert_eq $((${key:20:1}-${key:17:1})) ${key:3:1};M=4M_M4
 assert_eq ${key:11:1} `tr '[:lower:]' '[:upper:]'<<<${key:5:1}`
+assert_eq `head -c1<<<"printf Ditto"` ${key:2:1}
+assert_eq ${#key} `expr "$key" : "[^\I]\{0\}.[\m].\{2\}[\s].\{2\}[\r].\{10\}[\g][^\o]\{0\}[\m]."`
+assert_eq ${M/M/m} ${key:13:5}
+assert_eq $((${key:6:1}${key:9:1}-${key:12:2})) ${key:3:1}
+assert_eq ${key:5:1} ${relic:11:1}
 
-# assert_eq $((`printf '%d\n' "'${key:13:1}"`-`printf '%d\n' "'${key:0:1}"`)) $((${key:14:1}*-1))
-# assert_eq 34 = 4
+open_relic () { perl -e ' $k=$ARGV[0]; use MIME::Base64; $p=decode_base64($ARGV[1]); print $p ^ $k' $key $relic; }
 
-assert_eq 0 ----end----
+transform_well () {
+   echo a
+}
 
-## calculus
-# assert_eq ${key:3:1} $((${key:16:1}+${key:14:1}))
-
-## resemblance ??
-# assert_eq `head -c1<<<\`printf Ditto\`` ${key:2:1}
-# assert_eq "${key:9:1}${key:19:1}" `(tail -c3 | head -c 2) <<< "abcd"`
-# assert_eq `tr '3' 'e' <<< ${key:4:4}` "uper"
-
-perl -e ' $k=$ARGV[0]; use MIME::Base64; $p=decode_base64($ARGV[1]); print $p ^ $k' $key $relic
+open_relic
